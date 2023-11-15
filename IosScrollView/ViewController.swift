5a50d0fb2b4dd133b4bfbbe2cf024f8fdfc22edd
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2023-11-15 13:51:42 +0500

    Initial Commit

---
+++ IosScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosScrollView
+//
+//  Created by A'zamjon Abdumuxtorov on 15/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
